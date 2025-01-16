365f5eb8c1e94c34d1ac057db99e5cb5b8f484c2
unknown <jjnaula3@utpl.edu.ec>
2025-01-16 12:49:14 -0500

    cambiosss33

--- EjemploTres/src/paquete03/clase03.java
+++ EjemploTres/src/paquete03/clase03.java
@@ -7,7 +7,34 @@ package paquete03;
 /**
  *
  * @author utpl
+ * 
  */
+import org.apache.commons.lang3.math.NumberUtils;
+
+
 public class clase03 {
+  
     
+    public static void main(String[] args) {
+        
+       
+        String[] datoInicial = {"100", "90", "1n", "10", "H1", "H2"};
+        
+        
+        int[] datoFinal = new int[datoInicial.length];
+        
+      
+        for (int i = 0; i < datoInicial.length; i++) {
+       
+            datoFinal[i] = NumberUtils.toInt(datoInicial[i], 0);
+        }
+        
+     
+        for (int valor : datoFinal) {
+            System.out.print(valor + " ");
+        }
+    }
 }
+
+    
+
